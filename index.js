cc5d53a45d8494b1659c80c3870431f6d9a45c14
Zany-Dre <71964024+Zany-Dre@users.noreply.github.com>
2020-09-27 13:33:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -198,9 +198,9 @@ client.on('message', message =>{
                         }
                         if(counter == 0){
                             console.log(options.league);
-                  text.push('\n'+options.league+'  \n'+tod.getHours()+" : "+min+erg+" "+play.homeTeam.name +" : "+play.awayTeam.name+"\n");
+                  text.push('\n'+options.league+'  \n'+tod.getHours()+2+" : "+min+erg+" "+play.homeTeam.name +" : "+play.awayTeam.name+"\n");
                 }else{
-                    text.push(tod.getHours()+" : "+min+erg+" "+play.homeTeam.name +" : "+play.awayTeam.name+"\n"); 
+                    text.push(tod.getHours()+2+" : "+min+erg+" "+play.homeTeam.name +" : "+play.awayTeam.name+"\n"); 
                 }
                 counter++;
                  }
